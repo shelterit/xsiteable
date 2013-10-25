66f7682d1aefaa53f47e89cb46741170592f3683
ShelterIt <alexander.johannesen@gmail.com>
2013-10-25 12:52:56 +1100

    menu fiddle

--- application/website/search/index.php
+++ application/website/search/index.php
@@ -229,7 +229,7 @@ class xs_action_instance extends xs_Action_Webpage {
 
         $this->glob->page->total_time = sprintf ( "%01.3f", $end_time - $start_time ) ;
 
-        // echo "<pre>" ; print_r ( $result ) ; echo "</pre>" ; die() ;
+        echo "<pre>" ; print_r ( $result ) ; echo "</pre>" ; die() ;
 
         $this->glob->stack->add ( 'xs_result', $result ) ;
         // $this->glob->stack->add ( 'xs_result_score', $score ) ;
