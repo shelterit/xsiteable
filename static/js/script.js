e83c8bbf2948b7ba9dead44cabdfb870155b737f
ShelterIt <alexander.johannesen@gmail.com>
2014-01-14 10:26:39 +1100

    css fix for moving widgets

--- static/js/script.js
+++ static/js/script.js
@@ -582,3 +582,53 @@ var slug = function(Text) {
         .replace("/[^\w-]+/g",'') ;
 };
 
+// self-calling lambda to for jQuery shorthand "$" namespace
+(function($){
+    // document onReady wrapper
+    $().ready(function(){
+        // check for the nefarious IE
+        if($.browser.msie) {
+            // capture the file input fields
+            var fileInput = $('input[type="file"]');
+            // add presentational <span> tags "underneath" all file input fields for styling
+            fileInput.after(
+                $(document.createElement('span')).addClass('file-underlay')
+            );
+            // bind onClick to get the file-path and update the style <div>
+            fileInput.click(function(){
+                // need to capture $(this) because setTimeout() is on the
+                // Window keyword 'this' changes context in it
+                var fileContext = $(this);
+                // capture the timer as well as set setTimeout()
+                // we use setTimeout() because IE pauses timers when a file dialog opens
+                // in this manner we give ourselves a "pseudo-onChange" handler
+                var ieBugTimeout = setTimeout(function(){
+                    // set vars
+                    var filePath     = fileContext.val(),
+                        fileUnderlay = fileContext.siblings('.file-underlay');
+                    // check for IE's lovely security speil
+                    if(filePath.match(/fakepath/)) {
+                        // update the file-path text using case-insensitive regex
+                        filePath = filePath.replace(/C:\\fakepath\\/i, '');
+                    }
+                    // update the text in the file-underlay <span>
+                    fileUnderlay.text(filePath);
+                    // clear the timer var
+                    clearTimeout(ieBugTimeout);
+                }, 10);
+            });
+        }
+    });
+})(jQuery);
+
+function unslug(slug) {
+    slug = slug.replace(/\.[^/.]+$/, "");
+    var words = slug.split(/[\s-_]+/);
+
+    for(var i = 0; i < words.length; i++) {
+      var word = words[i];
+      words[i] = word.charAt(0).toUpperCase() + word.slice(1);
+    }
+
+    return words.join(' ');
+}
