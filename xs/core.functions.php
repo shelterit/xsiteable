4032e56844ccdd7a3b57096e4fc2bf6851fe7e88
ShelterIt <alexander.johannesen@gmail.com>
2014-02-06 17:18:18 +1100

    dms upgrade

--- xs/core.functions.php
+++ xs/core.functions.php
@@ -5,6 +5,50 @@
      * mash wouldn't be necessary. I'm sorry, I was in a hurry.
      */
 
+function php2js($a=false)
+{
+  if (is_null($a)) return 'null';
+  if ($a === false) return 'false';
+  if ($a === true) return 'true';
+  if (is_scalar($a))
+  {
+    if (is_float($a))
+    {
+      // Always use "." for floats.
+      $a = str_replace(",", ".", strval($a));
+    }
+
+    // All scalars are converted to strings to avoid indeterminism.
+    // PHP's "1" and 1 are equal for all PHP operators, but
+    // JS's "1" and 1 are not. So if we pass "1" or 1 from the PHP backend,
+    // we should get the same result in the JS frontend (string).
+    // Character replacements for JSON.
+    static $jsonReplaces = array(array("\\", "/", "\n", "\t", "\r", "\b", "\f", '"'),
+    array('\\\\', '\\/', '\\n', '\\t', '\\r', '\\b', '\\f', '\"'));
+    return '"' . str_replace($jsonReplaces[0], $jsonReplaces[1], $a) . '"';
+  }
+  $isList = true;
+  for ($i = 0, reset($a); $i < count($a); $i++, next($a))
+  {
+    if (key($a) !== $i)
+    {
+      $isList = false;
+      break;
+    }
+  }
+  $result = array();
+  if ($isList)
+  {
+    foreach ($a as $v) $result[] = php2js($v);
+    return '[ ' . join(', ', $result) . ' ]';
+  }
+  else
+  {
+    foreach ($a as $k => $v) $result[] = php2js($k).': '.php2js($v);
+    return '{ ' . join(', ', $result) . ' }';
+  }
+}
+
    function touch_it_good ( $filename ) {
        
         $out = array () ;
@@ -522,7 +566,7 @@ function recurse_array_key_value ( $source ) {
     return '<item name="'.urlsafe($source[0]).'" id="'.$source[0].'">'.$dest.'</item>' ;
 }
 
-function natsort2d( &$arrIn, $index = null )
+function natsort2d( &$arrIn, $index = null, $dir = 'asc' )
 {
     
     $arrTemp = array();
@@ -1182,4 +1226,13 @@ class SimpleImage {
       $this->image = $new_image;
    }      
  
+}
+
+
+
+
+function filesize_formatted ( $size ) {	
+    $units = array( 'b', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb', 'Yb');
+    $power = $size > 0 ? floor(log($size, 1024)) : 0;	
+    return number_format($size / pow(1024, $power), 2, '.', ',') . ' ' . $units[$power];
 }
