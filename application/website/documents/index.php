b8fb291d824e2a8d1799bd54337ffdb8457d9bef
ShelterIt <alexander.johannesen@gmail.com>
2014-02-12 10:47:13 +1100

    search for documents by topic id

--- application/website/documents/index.php
+++ application/website/documents/index.php
@@ -51,8 +51,13 @@ class xs_action_instance extends \xs\Action\Webpage {
                 // make list of one into one
                 $document = reset ( $documents ) ;
                 
+                if ( $document['type1'] !== $this->_type->doc || $document['type1'] !== $this->_type->doc_draft )
+                    $mode = 'not_found' ;
+                
                 $uid = substr ( $document['name'], strpos ( $document['name'], ':' ) + 1 ) ;
                 // debug ( $uid ) ;
+            } else {
+                $mode = 'not_found' ;
             }
             
         }
@@ -77,14 +82,24 @@ class xs_action_instance extends \xs\Action\Webpage {
                 
                 $mode = 'document' ;
                 
+                
+                
+                if ( $document['type1'] !== $this->_type->doc || $document['type1'] !== $this->_type->doc_draft )
+                    $mode = 'not_found' ;
+                
                 $version = trim ( $f->__fetch ( 'f2', '' ) ) ;
                 
                 if ( $version != '' ) {
                     $submode = true ;
                 }
                 
+            } else {
+                $mode = 'not_found' ;
             }
         }
+        
+        if ( $document['type1'] == null )
+            $mode = 'not_found' ;
         
         // Pull out all facets (well, first 9) from the request URI
         for ( $n=1; $n < 10; $n++) {
@@ -128,32 +143,34 @@ class xs_action_instance extends \xs\Action\Webpage {
                 $this->glob->page->relative_path = $path ;
                 break ;
                 
+            case 'not_found' :
+                
+                $this->set_template ( 'not_found' ) ;
+                $this->set_title ( 'Document not found' ) ;
+                
+                break ;
+                
             case 'document':
                 
                 // Yes, it's a document / URI; use new template
                 $this->set_template ( 'show' ) ;
-                
                 $this->set_title ( 'Document' ) ;
                 
-                if ( $submode ) {
-                    $this->set_title ( 'Document version' ) ;
-                    $this->set_template ( 'show-version' ) ;
-                }
-                
+                    
                 $p = pathinfo ( $document['original_path'] ) ;
-                
+
                 if ( ! isset ( $document['extension'] ) )
                     $document['extension'] = $p['extension'] ;
-                
+
                 if ( ! isset ( $document['basename'] ) )
                     $document['basename'] = $p['basename'] ;
-                
+
                 if ( ! isset ( $document['filename'] ) )
                     $document['filename'] = $p['filename'] ;
-                
+
                 $ext = $document['extension'] ;
                 $document['uid'] = $uid ;
-                
+
                 if ( ! isset ( $document['pub_format'] ) ) {
                     $document['pub_format'] = 'false' ;
                     if ( isset ( $this->glob->config['dms']['publish_in_pdf_format'] ) && $this->glob->config['dms']['publish_in_pdf_format'] == '1' )
@@ -170,18 +187,18 @@ class xs_action_instance extends \xs\Action\Webpage {
                         }
                     }
                 }
-                
+
                 $icon = '24x24/mimetypes/binary.png' ;
-                
+
                 $document['final_path'] = $dms->get_dir_structure ( $uid, null ) ;
                 $document['home_path'] = $dms->get_dir_structure ( $uid, '' ) ;
                 $document['final_dir'] = $document['final_path'].'/'.$uid.'.'.$ext ;
                 $document['final_dir_txt'] = $document['final_path'].'/'.$uid.'.txt' ;
                 $document['final_dir_html'] = $document['final_path'].'/'.$uid.'.html' ;
-                
+
                 $document['final_www_html'] = $this->glob->config['dms']['destination_uri'] .
                     $document['home_path'] .'/'. $uid.'.html' ;
-                
+
                 $file_exist_original = file_exists ( $document['original_path'] ) ;
                 $file_exist_dest = @file_exists ( @$document['final_dir'] ) ;
                 $file_exist_html = @file_exists ( @$document['final_dir_html'] ) ;
@@ -202,77 +219,77 @@ class xs_action_instance extends \xs\Action\Webpage {
                     else
                         echo '<p>Document is NOT controlled; panic!</p>' ;
                 }
-                
+
                 if ( $file_exist_dest === false ) {
                     // copy original to dest
                     echo '<p>Copy original to destination ['.$document['final_dir'].'].</p>' ;
                     if ( isset ( $document['controlled'] ) && $document['controlled'] == 'true' )
                         echo '<p>Document is controlled, so if there\'s no current document, panic!</p>' ;
                 }
-                
+
                 if ( $file_exist_html === false ) {
                     // create HTML
                     echo '<p>No HTML; convert original to HTML ['.$document['final_dir_html'].']</p>' ;
                 }
-                
+
                 if ( $file_exist_txt === false ) {
                     // create TXT
                     echo '<p>No TXT; convert HTML to TXT ['.$document['final_dir_txt'].'], invoke spidering and indexing</p>' ;
                 }
-                
-                
+
+
                 */
-                
-                
-                
-                
-                
+
+
+
+
+
                 if ( isset ( $this->glob->config['dms'][$ext.'.icon'] ) )
                     $icon = $this->glob->config['dms'][$ext.'.icon'] ;
 
                 $document['icon'] = $icon ;
-                
+
                 $current_facets = array () ;
                 $path = '' ;
-                
+
                 if ( isset ( $document['relative_path'] ) )
                     foreach ( explode ( '/', $document['relative_path'] ) as $i ) {
                         $path .= $i . '/' ;
                         $current_facets[urlsafe(substr ( $path, 0, -1 ))] = $i ;
                     }
-                
+
                 // if ( ! isset ( $document['next_review_date'] ) )
                 //     $document['next_review_date'] = date ( "Y-m-d", mktime(0, 0, 0, date("m"),   date("d"),   date("Y")+1) ) ;
- 
-                
+
+
                 if ( ! isset ( $document['publish_date'] ) && $file_exist_original )
                     $document['publish_date'] = date ( "Y-m-d", filemtime ( $document['original_path'] ) ) ;
-                
+
                 if ( ! isset ( $document['state'] ) )
                     $document['state'] = '' ;
-                
-                
+
+
                 if ( isset ( $fields['approved_by'] ) ) {
-                    
+
                     // yes, someone is approving this document
                     $document['state'] = 'approved' ;
                     $document['approved_by'] = $fields['approved_by'] ;
                     $document['who'] = $this->glob->user->id ;
-                    
+
                     $this->glob->tm->update ( $document ) ;
                 }
-                
+
                 if ( isset ( $document['approved_by'] ) ) {
                     $ab = $this->glob->tm->query ( array ( 'id' => $document['approved_by'] ) ) ;
                     $t = reset ( $ab ) ;
                     $document['approved_by_label'] = $t['label'] ;
                 }
-                
+
                 $this->glob->stack->add ( 'xs_document', $document ) ;
 
 
                 if ( isset ( $document['words_important'] ) ) {
-                    
+
                     // Find all important words in all documents
                     $tmp = unserialize ( $document['words_important'] ) ;
 
@@ -296,21 +313,21 @@ class xs_action_instance extends \xs\Action\Webpage {
                     $this->glob->stack->add ( 'xs_content', array (
                         'keywords' => $res
                     ) ) ;                
-                    
+
                 }
-                
+
 
                 $id = $document['id'] ;
 
 
                 $tm = $this->_get_module ( 'topic_maps' ) ;
-                
+
                 $this->glob->stack->add ( 'xs_assoc_tags', $tm->get_assoc ( array (
                     'lookup' => $id,
                     'type' => $this->_type->has_tag,
                     'filter' => $this->_type->_tag,
                 ) ) ) ;
-                
+
                 $this->glob->stack->add ( 'xs_assoc_controlled_tags', $tm->get_assoc ( array (
                     'lookup' => $id,
                     'type' => $this->_type->has_controlled_tag,
@@ -322,17 +339,17 @@ class xs_action_instance extends \xs\Action\Webpage {
                     'type' => $this->_type->has_owner,
                     'filter' => $this->_type->_user,
                 ) ) ) ;
-                
+
                 $this->glob->stack->add ( 'xs_assoc_author', $tm->get_assoc ( array (
                     'lookup' => $id,
                     'type' => $this->_type->has_author,
                     'filter' => $this->_type->_user,
                 ) ) ) ;
-                
+
 
                 // log this reading
                 $this->log ( 'READ', $path ) ;
-                
+
 
                 // comments?
                 $comments = $this->_get_module ( 'comments' ) ;
